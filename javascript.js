2d63e2412816867bbdf5e3fd44fc7b1d6ab46f3c
nicholaspeck01 <nicholaspeck01@gmail.com>
2022-06-22 15:04:04 -0700

    created 'rps' variable that is an array of rock, paper and scissors. by doing this computer selection will be either rock, paper or scissors. this makes it easy to write the play round function.

--- javascript.js
+++ javascript.js
@@ -1,17 +1,23 @@
-let playerSelection = prompt("I challenge you to a game of Rock Paper scissors! input your choice!");
-let computerSelection = computerPlay();
-var rps = ["rock","paper","scissors"];
+//let playerSelection = prompt("I challenge you to a game of Rock Paper scissors! input your choice!");
 
+var rps = ["rock","paper","scissors"];
+computerSelection = rps[computerPlay()]
+let playerScore =0;
+let computerScore =0;
 function computerPlay(){
     return Math.floor(Math.random() * 3);
+
 }
 
 
 
 function playRound(playerSelection, computerSelection) {
-    // your code here!
+    if (playerSelection == "rock" && computerSelection == "paper"){
+        console.log("computer chooses: " + computerSelection + "!\n" + "you chose:" + playerSelection + "!\n" );
+    }
+
   }
-  
+  console.log(computerSelection);
 
   //console.log(playRound(playerSelection, computerSelection));
   
