f640785e15849cff767254a552b042bcd980f3a5
nicholaspeck01 <nicholaspeck01@gmail.com>
2022-06-22 14:33:49 -0700

    created computerPlay function that chooses a random number between 0 and 2. I plan to use this number to call an array which selects from rock paper or scissors.

---
+++ javascript.js
@@ -0,0 +1,17 @@
+let playerSelection = prompt("I challenge you to a game of Rock Paper scissors! input your choice!");
+let computerSelection = computerPlay();
+var rps = ["rock","paper","scissors"];
+
+function computerPlay(){
+    return Math.floor(Math.random() * 3);
+}
+
+
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+  }
+  
+
+  //console.log(playRound(playerSelection, computerSelection));
+  
